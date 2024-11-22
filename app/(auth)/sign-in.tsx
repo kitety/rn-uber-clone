4fc7c61d59557ebc0e66585267f0e981eec27f4e
kitety <aizaizuori@foxmail.com>
2024-11-22 23:16:04 +0800

    feat: sign in ui

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,59 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useReactive } from 'ahooks';
+import { Link } from 'expo-router';
+import { Image, ScrollView, Text, View } from 'react-native';
+import CustomButton from '~/components/customButton';
+import InputField from '~/components/inputField';
+import OAuth from '~/components/oauth';
+import { icons, images } from '~/constants';
 
 const SignIn = () => {
+  const state = useReactive({
+    email: '',
+    password: '',
+  });
+
+  const onSignInPress = () => {
+    console.log('state', state);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative h-[250px] w-full">
+          <Image className="z-0 h-[250px] w-full" source={images.signUpCar} />
+          <Text className="bottom-5 left-5 font-JakartaSemiBold text-2xl text-black">Welcome</Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            icon={icons.email}
+            label="Email"
+            placeholder="Enter your email"
+            value={state.email}
+            onChangeText={(text) => {
+              state.email = text;
+            }}
+          />
+          <InputField
+            secureTextEntry
+            icon={icons.lock}
+            label="Password"
+            placeholder="Enter your password"
+            value={state.password}
+            onChangeText={(text) => {
+              state.password = text;
+            }}
+          />
+          <CustomButton className="mt-6" title="Sign In" onPress={onSignInPress} />
+          <OAuth />
+          <Link
+            className="mt-10 flex flex-row text-center text-lg text-general-200"
+            href="/sign-up">
+            <Text>Don't have an account?</Text>&nbsp;
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
--- components/oauth.tsx
+++ components/oauth.tsx
@@ -1,6 +1,11 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { icons } from '~/constants';
+import CustomButton from './customButton';
 
 const OAuth = () => {
+  const handleGoogleSignIn = async () => {
+    console.log('google');
+  };
   return (
     <View>
       <View className="mt-4 flex-row items-center justify-center gap-3">
@@ -8,6 +13,16 @@ const OAuth = () => {
         <Text className="text-lg">OR</Text>
         <View className="h-hairline flex-1 bg-general-100" />
       </View>
+      <CustomButton
+        bgVariant="outline"
+        className="mt-5 w-full shadow-none"
+        textVariant="primary"
+        title="Continue with Google"
+        IconLeft={() => (
+          <Image className="mx-2 h-5 w-5" resizeMode="contain" source={icons.google} />
+        )}
+        onPress={handleGoogleSignIn}
+      />
     </View>
   );
 };
